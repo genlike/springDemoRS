d09f0283ed121b7e033a9eec9f49c7c56d9ca130
genlike <ricardo.f.oliveira.da.silva@tecnico.ulisboa.pt>
2024-01-29 01:22:30 +0000

    initial commit

---
+++ src/main/java/com/example/springDemoRS/SpringDemoRsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springDemoRS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoRsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoRsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springDemoRS/SpringDemoRsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springDemoRS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoRsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
